3c4ed8e344c1ad922f86841dc7dd657ed50ba4bd
markymarcsons <marc_pomer@yahoo.de>
2021-04-29 16:34:38 +0200

    Infobox

--- src/components/info.js
+++ src/components/info.js
@@ -9,7 +9,14 @@ function Infobox() {
             <Card className="Card">
                 <Card.Body ClassName="Card Card-Body">
                     <Card.Title card card-title>Information</Card.Title>
-                    <Card.Text>All the information you need</Card.Text>
+                    <Card.Text>
+                    Over the last decade Crowdfunding has become a key financing channel for entrepreneurs, artists and social projects. 
+                    subscription based crowdfunding platforms such as Patreon turn the single interaction between creator and crowd into an ongoing relationship.
+                    The goal of this Survey is to investigate what drives the crowd wether to keep or stop subscribing to creators.
+                    This survey is completely anonymous and will take about 7 mins.
+                    For further questions feel free to contact me under following email address:
+                    </Card.Text>
+                    <Card.Text>s2198395@stud.uni-frankfurt.de</Card.Text>
                 </Card.Body>
             </Card>
         </div>
