fe561a531ad9c82d701e9ac49d47c1d2ec23c52a
markymarcsons <marc_pomer@yahoo.de>
2021-05-03 20:20:54 +0200

    info card font-size

--- src/components/info.js
+++ src/components/info.js
@@ -9,12 +9,12 @@ function Infobox() {
                     <Card.Title className="text-center"><h1>Information</h1></Card.Title>
                     <Card.Text><p>
                     Over the last decade Crowdfunding has become a key financing channel for entrepreneurs, artists and social projects. 
-                    subscription based crowdfunding platforms such as Patreon turn the single interaction between creator and crowd into an ongoing relationship.
+                    subscription based crowdfunding platforms such as Patreon turn the single interaction between these creators and their crowd into an ongoing relationship.
                     The goal of this Survey is to investigate what drives people to support these creators.
                     This survey is completely anonymous and will take about 7-10 mins.</p>
                     For any further questions feel free to contact me under following email address:
                     </Card.Text>
-                    <Card.Text>s2198395@stud.uni-frankfurt.de</Card.Text>
+                    <Card.Text>thesis@anonymized.org</Card.Text>
                 </Card.Body>
                 
             </Card>
