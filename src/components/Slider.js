73555f59a75d1ac210d9f5ce573cd4ee92b6d557
markymarcsons <marc_pomer@yahoo.de>
2021-05-07 01:04:25 +0200

    secondary button/ slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,15 +6,34 @@ import RangeSlider from 'react-bootstrap-range-slider';
 const Slider = () => {
 
     const [ value, setValue ] = useState(50);
-  
-    return (
-      <RangeSlider
-        value={value}
-        onChange={e => setValue(e.target.value)}
-        step={10}
-      />
+    const [ finalValue, setFinalValue ] = useState(null);
+
+    const Rewards = () => {
+      if(finalValue <= 10) {
+        let reward = 'bronze badge';
+        return reward
+      }
+      if(finalValue > 10 && finalValue <=50){
+        let reward = 'silver badge';
+        return reward
+      }
+      else {
+        let reward = 'golden badge';
+        return reward
+      };
+    };
+
+
+    return(
+      <>
+        <RangeSlider
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          onAfterChange={e => setFinalValue(e.target.value)}/>
+          <div>Subscription Fee: {finalValue}$</div>
+          <div>Receiving Rewards: {Rewards}</div>
+      </>
     );
-  
   };
 
   export default Slider;
